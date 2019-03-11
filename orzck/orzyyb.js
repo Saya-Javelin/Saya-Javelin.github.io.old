ff1d0d547f18296c222cc84927f332c40ae52e36
Saya-Javelin <913759697@qq.com>
2019-03-11 15:21:30 +0800

    Site updated: 2019-03-11 15:21:29

--- orzck/orzyyb.js
+++ orzck/orzyyb.js
@@ -5,8 +5,8 @@ document.getElementById("mainarea").innerHTML="\
 <span style=\"font-size: 128px; color:rgb(66,133,244)\">C</span>\
 <span style=\"font-size: 128px; color:rgb(234,67,53)\">K</span>\
 <span style=\"font-size: 128px; color:rgb(251,188,5)\"> O</span>\
-<span style=\"font-size: 128px; color:rgb(66,133,244)\">R</span>\
-<span style=\"font-size: 128px; color:rgb(52,168,83)\">Z</span>\
+<span style=\"font-size: 128px; color:rgb(66,133,244)\">r</span>\
+<span style=\"font-size: 128px; color:rgb(52,168,83)\">z</span>\
 </p>\
 <p align=\"center\">\
 <div style=\"background:white;font: 10px/6px monospace;width:520px;\"></div>\
