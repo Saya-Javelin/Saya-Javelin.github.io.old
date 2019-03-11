3659714c91af3e5f0035aad17f0478b264b14962
Saya-Javelin <913759697@qq.com>
2019-03-11 15:18:32 +0800

    Site updated: 2019-03-11 15:18:32

--- orzck/orzyyb.js
+++ orzck/orzyyb.js
@@ -2,11 +2,11 @@
 function orzyyb(){
 document.getElementById("mainarea").innerHTML="\
 <p align=\"center\">\
-<span style=\"font-size: 128px; color:rgb(66,133,244)\">O</span>\
-<span style=\"font-size: 128px; color:rgb(234,67,53)\">r</span>\
-<span style=\"font-size: 128px; color:rgb(251,188,5)\">z</span>\
 <span style=\"font-size: 128px; color:rgb(66,133,244)\">C</span>\
-<span style=\"font-size: 128px; color:rgb(52,168,83)\">K</span>\
+<span style=\"font-size: 128px; color:rgb(234,67,53)\">K</span>\
+<span style=\"font-size: 128px; color:rgb(251,188,5)\"> O</span>\
+<span style=\"font-size: 128px; color:rgb(66,133,244)\">R</span>\
+<span style=\"font-size: 128px; color:rgb(52,168,83)\">Z</span>\
 </p>\
 <p align=\"center\">\
 <div style=\"background:white;font: 10px/6px monospace;width:520px;\"></div>\
