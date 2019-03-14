20ce4001a91bff900d7718733d94b9cd394cd2ef
Saya-Javelin <913759697@qq.com>
2019-03-14 09:56:07 +0800

    Site updated: 2019-03-14 09:56:04

--- orzck/orzyyb.js
+++ orzck/orzyyb.js
@@ -46,9 +46,10 @@ if(cnt==10)alert('CK：虽然我是大佬，但是你这样一直膜我是会掉
 if(cnt==20)alert('CK：MDZZ你怎么还在膜信不信我把你从5楼扔下去');
 if(cnt==50)alert('CK：没错我就是这么强，让你好好膜');
 if(cnt==50)delta=5,document.getElementById("ORZORZORZ").innerHTML="继续膜拜"+delta+"次";
+if(cnt==250)delta=10,document.getElementById("ORZORZORZ").innerHTML="继续膜拜"+delta+"次";
 if(cnt==1000)alert('CK：你真棒，我感受到了我又变强了，让你一次多膜一点');
 if(cnt>=1000){
-delta=Math.floor(Math.random()*100+5);
+delta=Math.floor(Math.random()*100+Math.random()*8);
 document.getElementById("ORZORZORZ").innerHTML="继续膜拜"+delta+"次";
 }
 if(cnt>=10000){
